2ec1e8168379413c1790f0ae2fcb90b8c821e290
Gregorius Dion <93753458+GregoriusDion@users.noreply.github.com>
2026-06-15 09:06:46 +0700

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,4 @@
 <?php
-// File: includes/footer.php
 ?>
 <div style="text-align:center; padding: 20px; color: #aaa; font-size: 13px; margin-top: 20px;">
     RestoApp © <?= date('Y') ?> — Dibuat dengan ❤️ menggunakan PHP + MySQL
